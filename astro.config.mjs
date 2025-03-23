e739b75bbd4f5859c8842b711e17b43d43bc5531
Deepak Surya <ideepaksuryas@gmail.com>
2025-03-23 21:51:34 +0530

    feat: update astro config and remove workflow yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://yoursite.com', // Replace with your actual domain
   // Optional: Use the following if you plan to deploy to Github Pages
-  site: "https://dsuryas.github.io/",
-  base: "/dss-website",
+  site: "https://dsuryas.github.io/", // Put with your actual domain
+  base: "/dss-website", // This should match your repository name
+  outDir: "./dist", // This is the default output directory
   integrations: [],
   markdown: {
     shikiConfig: {
